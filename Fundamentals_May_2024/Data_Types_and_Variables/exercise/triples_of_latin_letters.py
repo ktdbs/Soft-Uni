3f735d98acd319dc04a8041a090977c48bdca277
ktdbs <17827562+ktdbs@users.noreply.github.com>
2024-04-14 14:09:16 +0300

    first task of current lecture

--- Fundamentals_May_2024/Data_Types_and_Variables/exercise/triples_of_latin_letters.py
+++ Fundamentals_May_2024/Data_Types_and_Variables/exercise/triples_of_latin_letters.py
@@ -1,3 +1,8 @@
 
+num_of_letters = int(input())
 
+for i1 in range(0,num_of_letters):
+    for i2 in range(0,num_of_letters):
+        for i3 in range(0,num_of_letters):
+            print(f'{chr(97 +i1)}{chr(97+i2)}{chr(97+i3)}')
 
